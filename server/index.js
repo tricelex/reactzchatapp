d883b1654562aabd47db292133151cb622fee128
Chuckz Okoye <tricelex@gmail.com>
2019-12-31 18:57:25 +0100

    change

--- server/index.js
+++ server/index.js
@@ -18,30 +18,31 @@ mongoose.connect(uri, {
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 io.on('connection', socket => {
-  // get the last 10 messages from the database.
+  // Get the last 10 messages from the database.
   Message.find()
     .sort({ createdAt: -1 })
     .limit(10)
     .exec((err, messages) => {
       if (err) return console.error(err);
 
-      // send the latest messages to the user
+      // Send the last messages to the user.
       socket.emit('init', messages);
     });
 
+  // Listen to connected users for a new message.
   socket.on('message', msg => {
-    // create a mesage with the content and the name of the user
+    // Create a message with the content and the name of the user.
     const message = new Message({
       content: msg.content,
       name: msg.name
     });
 
-    // save the message to the database
+    // Save the message to the database.
     message.save(err => {
       if (err) return console.error(err);
     });
 
-    // NOtify all other users about a new message
+    // Notify all other users about a new message.
     socket.broadcast.emit('push', msg);
   });
 });
